bacec7168767e0335b9e85078f8cb50c30a318be
Mark Varabyou <vorobyovmark@gmail.com>
2023-12-26 12:06:19 -0800

    new nvim init

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,41 @@
+-- Setup package manager (lazy.nvim)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to set `mapleader` before lazy so your mappings are correct
+-- vim.g.mapleader = " " 
+
+-- Install packages
+require("lazy").setup({
+  --- Uncomment these if you want to manage LSP servers from neovim
+  -- {'williamboman/mason.nvim'},
+  -- {'williamboman/mason-lspconfig.nvim'},
+
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
+})
+
+-- LSP zero setup - lazy man's LSP and autocomplete
+local lsp_zero = require('lsp-zero')
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- Setup for my favorite languages
+local lspconfig = require('lspconfig')
+lspconfig.ocamllsp.setup{}
+lspconfig.tsserver.setup{}
+
